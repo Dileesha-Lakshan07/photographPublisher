08a49fe3c822d0f27a471b4c0d4e541ea5975616
DILEESHA\dlaks <dlakshansamaraweera@gmail.com>
2024-10-07 01:03:57 +0530

    add react project and add some jsx files for pages.

---
+++ photopub/src/pages/Authors.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Authors = () => {
+  return <div></div>;
+};
+
+export default Authors;
---
+++ photopub/src/pages/CategoryPosts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CategoryPosts = () => {
+  return <div></div>;
+};
+
+export default CategoryPosts;
